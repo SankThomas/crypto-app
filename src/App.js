6e981eeafebcaabfec6ce8821afa7a1a3480a3bf
Thomas Sankara <thomassankara74@gmail.com>
2022-02-05 01:06:30 +0300

    set up react-routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import FetchCoins from "./components/FetchCoins"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import SingleCoin from "./components/SingleCoin"
+import Error from "./components/Error"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="bg-slate-900 p-5">
+      <main className="lg:max-w-5xl lg:mx-auto">
+        <h1 className="text-4xl text-center lg:text-6xl text-gray-200 font-bold mb-5">
+          Latest in the Crypto Space
+        </h1>
+        <p className="text-center text-slate-300 mb-5">
+          View information about the cryto space including market prices, all
+          updated every minute with the freshest data from the{" "}
+          <a
+            href="https://coingecko.com"
+            target="_blank"
+            rel="noopenner noreferrer"
+            className="text-white border-b border-dashed"
+          >
+            coingecko API.
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<FetchCoins />}></Route>
+            <Route path="/:id" element={<SingleCoin />}></Route>
+            <Route path="*" element={<Error />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
